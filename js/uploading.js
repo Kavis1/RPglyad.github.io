43cb149289ff107d8919f000d81b47d8edc113a6
Kavis1 <73421196+Kavis1@users.noreply.github.com>
2023-06-07 21:20:08 +0200

    Delete js directory

--- js/uploading.js
+++
@@ -1,7 +0,0 @@
-function UploadVideo() {
-  var playerContainer = document.querySelector('.player_container');
-  var videoTitle = document.querySelector('.video_title');
-
-  playerContainer.classList.add('error');
-  videoTitle.innerHTML = 'There has been an ERROR :(';
-}
