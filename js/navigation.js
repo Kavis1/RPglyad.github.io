fae5624acb9ac50312a32b92008956c03f04b750
Kavis1 <73421196+Kavis1@users.noreply.github.com>
2023-06-07 21:42:04 +0200

    Update navigation.js

--- js/navigation.js
+++ js/navigation.js
@@ -12,62 +12,62 @@ function MoreMenu() {
    dropdownMenu.classList.toggle('show');
 }
 function TransitionVideo1() {
-   window.location.href = "../html/player_v1.html";
+   window.location.href = "html/player_v1.html";
 }
 function TransitionVideo2() {
-   window.location.href = "../html/player_v2.html";
+   window.location.href = "html/player_v2.html";
 }
 function TransitionVideo3() {
-   window.location.href = "../html/player_v3.html";
+   window.location.href = "html/player_v3.html";
 }
 function TransitionVideo4() {
-   window.location.href = "../html/player_v4.html";
+   window.location.href = "html/player_v4.html";
 }
 function TransitionVideo5() {
-   window.location.href = "../html/player_v5.html";
+   window.location.href = "html/player_v5.html";
 }
 function TransitionVideo6() {
-   window.location.href = "../html/player_v6.html";
+   window.location.href = "html/player_v6.html";
 }
 function TransitionVideo7() {
-   window.location.href = "../html/player_v7.html";
+   window.location.href = "html/player_v7.html";
 }
 function TransitionVideo8() {
-   window.location.href = "../html/player_v8.html";
+   window.location.href = "html/player_v8.html";
 }
 function TransitionVideo9() {
-   window.location.href = "../html/player_v9.html";
+   window.location.href = "html/player_v9.html";
 }
 function TransitionVideo10() {
-   window.location.href = "../html/player_v10.html";
+   window.location.href = "html/player_v10.html";
 }
 function TransitionVideo11() {
-   window.location.href = "../html/player_v11.html";
+   window.location.href = "html/player_v11.html";
 }
 function TransitionVideo12() {
-   window.location.href = "../html/player_v12.html";
+   window.location.href = "html/player_v12.html";
 }
 function TransitionVideo13() {
-   window.location.href = "../html/player_v13.html";
+   window.location.href = "html/player_v13.html";
 }
 function TransitionVideo14() {
-   window.location.href = "../html/player_v14.html";
+   window.location.href = "html/player_v14.html";
 }
 function TransitionVideo15() {
-   window.location.href = "../html/player_v15.html";
+   window.location.href = "html/player_v15.html";
 }
 function TransitionVideo16() {
-   window.location.href = "../html/player_v16.html";
+   window.location.href = "html/player_v16.html";
 }
 function TransitionVideo17() {
-   window.location.href = "../html/player_v17.html";
+   window.location.href = "html/player_v17.html";
 }
 function TransitionVideo18() {
-   window.location.href = "../html/player_v18.html";
+   window.location.href = "html/player_v18.html";
 }
 function TransitionVideo19() {
-   window.location.href = "../html/player_v19.html";
+   window.location.href = "html/player_v19.html";
 }
 function TransitionVideo20() {
-   window.location.href = "../html/player_v20.html";
+   window.location.href = "html/player_v20.html";
 }        
